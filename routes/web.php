5b55ff08bbe8223163660addd3a275808d964b66
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2025-02-10 16:12:26 +0900

    ルートとconfigの設定

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/redirect/{provider}', [SocialiteController::class, 'redirect']);
+Route::get('/callback/{provider}', [SocialiteController::class, 'callback']);
