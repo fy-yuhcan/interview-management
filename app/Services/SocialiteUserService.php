1d910af8b0773047079e34c53e11f11d20e8e8ea
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2025-02-12 00:52:44 +0900

    userモデルのマスアサインメントの制限を修正

--- app/Services/SocialiteUserService.php
+++ app/Services/SocialiteUserService.php
@@ -7,16 +7,16 @@ use App\Models\User;
 class SocialiteUserService
 {
     //userが存在するか確認し、存在しない場合は新規作成するサービスクラス
-    public function findOrCreateUser($providerUser,$provider)
+    public function findOrCreateUser($user,$provider)
     {
         return User::firstOrCreate(
-            ['email' => $providerUser->getEmail()],
+            ['email' => $user->getEmail()],
             [
-                'name' => $providerUser->getName(),
+                'name' => $user->getName(),
                 'provider' => $provider,
-                'provider_id' => $providerUser->getId(),
-                'avatar' => $providerUser->getAvatar(),
-                'token' => $providerUser->token,
+                'provider_id' => $user->getId(),
+                'avatar' => $user->getAvatar(),
+                'token' => $user->token,
             ]
             );
     }
