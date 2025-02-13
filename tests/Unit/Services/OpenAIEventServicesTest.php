fb283a89600e80c348a0a691248d81a4736e332c
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2025-02-13 22:18:54 +0900

    openAIAPIのクライアントをインストールし、レスポンスを受け取ることができること確かめるテストを作成

--- tests/Unit/Services/OpenAIEventServicesTest.php
+++ tests/Unit/Services/OpenAIEventServicesTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Unit;
 
+use Illuminate\Support\Facades\Http;
+use OpenAI\Laravel\Facades\OpenAI;
 use PHPUnit\Framework\TestCase;
 
 class OpenAIEventServicesTest extends TestCase
@@ -9,8 +11,30 @@ class OpenAIEventServicesTest extends TestCase
     /**
      * A basic unit test example.
      */
-    public function test_example(): void
+    public function test_create_prompt_to_OpenAI(): void
     {
-        $this->assertTrue(true);
+        // テスト用テキスト
+        $prompt = '2月10日16時に打ち合わせを行います。';
+
+        //fakeを使ってOpenAI::createPromptメソッドをモック化
+        $fakeresponse = [
+            'choices' => [
+                [
+                    'text' => '2月10日16時に打ち合わせを行います。',
+                ],
+            ],
+        ];
+
+        //shouldReceiveで受け取る内容を指定
+        OpenAI::shouldReceive('createPrompt')
+            ->once()
+            ->with($prompt)
+            ->andReturn($fakeresponse);
+        
+        // OpenAI API へのリクエスト
+        $response = OpenAI::createPrompt($prompt);
+
+        $this->assertEquals($fakeresponse, $response);
+
     }
 }
