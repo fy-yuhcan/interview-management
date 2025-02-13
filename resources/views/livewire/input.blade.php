19ca46840ad973de6c68976205e6d9f2b7269dad
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2025-02-13 20:04:42 +0900

    livewireとtestの作成

--- resources/views/livewire/input.blade.php
+++ resources/views/livewire/input.blade.php
@@ -1,3 +1,14 @@
 <div>
-    {{-- In work, do what you enjoy. --}}
+    <form wire:submit.prevent="submit">
+        <textarea wire:model="prompt" placeholder="イベントの内容を入力してください"></textarea>
+        <button type="submit">イベント作成</button>
+    </form>
+
+    @if (session()->has('message'))
+    <div style="color: green;">{{ session('message') }}</div>
+    @endif
+
+    @if (session()->has('error'))
+    <div style="color: red;">{{ session('error') }}</div>
+    @endif
 </div>
