4bb85b806ca7f16c65f63a95a69bd29f60c72cdf
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2025-02-16 15:51:47 +0900

    Googleカレンダーから全てのイベントを取得するテストを作成

--- tests/Unit/Services/CreateEventFromGoogleCalendarTest.php
+++ tests/Unit/Services/CreateEventFromGoogleCalendarTest.php
@@ -5,7 +5,10 @@ namespace Tests\Unit;
 use App\Models\Event;
 use App\Models\User;
 use App\Services\EventCreateService;
+use App\Services\GoogleCalendarService;
+use Google\Service\Calendar;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Mockery;
 use Tests\TestCase;
 
 class CreateEventFromGoogleCalendarTest extends TestCase
@@ -76,5 +79,76 @@ class CreateEventFromGoogleCalendarTest extends TestCase
 
         $this->assertInstanceOf(Event::class, $event);
 
+    }
+
+    //Googleカレンダーから全てのイベントを取得するテスト
+    public function test_get_all_events_from_google_calendar()
+    {
+        //fakeオブジェクトを使ってGoogleカレンダーAPIをモック化
+        $fakeGoogleEvent = new class {
+            public function getSummary()
+            {
+                return '会議';
+            }
+            public function getStart()
+            {
+                return '2025-02-20 10:00:00';
+            }
+            public function getEnd()
+            {
+                return '2025-02-20 11:00:00';
+            }
+            public function getReservationTime()
+            {
+                return '2025-02-20 09:00:00';
+            }
+            public function getStatus()
+            {
+                return '予定';
+            }
+            public function getUrl()
+            {
+                return 'https://example.com';
+            }
+            public function getDetail()
+            {
+                return 'プロジェクト進捗会議';
+            }
+        };
+
+        $fakeEventsArray = [$fakeGoogleEvent];
+
+
+        //googleカレンダーのlistEvents()メソッドがgetItems()メソッドを持つのでスタブで再現した
+        $fakeEventsResponse = new class($fakeEventsArray) {
+            public $items;
+            public function __construct($items)
+            {
+                $this->items = $items;
+            }
+            public function getItems()
+            {
+                return $this->items;
+            }
+        };
+
+        //googleカレンダーAPIをモック化
+        $fakeEventsResource = Mockery::mock();
+        
+        //listEventsメソッドが呼ばれたら$fakeEventsResponseを返すように設定
+        $fakeEventsResource->shouldReceive('listEvents')->andReturn($fakeEventsResponse);
+
+        //Google\service\Calendarのeventsプロパティに$fakeEventsResourceを設定
+        $mockCalendarService = Mockery::mock(Calendar::class);
+        $mockCalendarService->events = $fakeEventsResource;
+
+        //GoogleCalendarServiceのインスタンスを生成して、getEventsメソッドを実行
+        $service = new GoogleCalendarService($mockCalendarService);
+
+        $events = $service->getEvents('primary');
+
+        $this->assertEquals($fakeEventsArray, $events);
+        $this->assertIsArray($events);
+        $this->assertEquals($fakeGoogleEvent->getSummary(), $events[0]->getSummary());
     }
 }
