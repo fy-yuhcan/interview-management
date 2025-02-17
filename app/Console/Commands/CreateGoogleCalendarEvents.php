53c55d64617c821d13fdc8f4ce24823559e168bc
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2025-02-17 22:27:28 +0900

    コマンドの実行内容を書いた

--- app/Console/Commands/CreateGoogleCalendarEvents.php
+++ app/Console/Commands/CreateGoogleCalendarEvents.php
@@ -2,6 +2,9 @@
 
 namespace App\Console\Commands;
 
+use App\Models\User;
+use App\Services\EventCreateService;
+use App\Services\GoogleCalendarService;
 use Illuminate\Console\Command;
 
 class CreateGoogleCalendarEvents extends Command
@@ -18,13 +21,27 @@ class CreateGoogleCalendarEvents extends Command
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'googleカレンダーからイベントを取得してDBに保存する';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        //
+        // tokenがあるユーザーを取得
+        $users = User::WhereNotNull('token')->get();
+        
+        // イベントを作成するサービスクラス
+        $createdEvents = new EventCreateService();
+
+        // ユーザーごとにイベントを取得してDBに保存
+        foreach ($users as $user) {
+            $service = new GoogleCalendarService($user->token);
+            $events = $service->getFormattedEvents();
+    
+            foreach ($events as $data) {
+                $createdEvents->createEvent($data, $user->id);
+            }
+        }
     }
 }
