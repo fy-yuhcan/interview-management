d49438a0c24127473d845397897defec5d352596
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2025-02-18 04:31:11 +0900

    検証ルートの削除

--- routes/web.php
+++ routes/web.php
@@ -10,15 +10,3 @@ Route::get('/', function () {
 Route::view('/login', 'auth.login')->name('login');
 Route::get('/redirect/{provider}', [SocialiteController::class, 'redirect'])->name('redirect');
 Route::get('/login/{provider}/callback', [SocialiteController::class, 'callback'])->name('callback');
-
-Route::get('dev/google-import-test', function () {
-    $user = \App\Models\User::find(1);
-    $service = new \App\Services\GoogleCalendarService($user->token);
-    $events = $service->getFormattedEvents();
-
-    foreach ($events as $data) {
-        (new \App\Services\EventCreateService())->createEvent($data, $user->id);
-    }
-
-    dd('Imported events: ', $events);
-});
