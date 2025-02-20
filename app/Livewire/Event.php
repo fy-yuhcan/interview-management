8080fbdf6cc3cfd467c9c991dda5305e4a07ca8e
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2025-02-20 11:35:09 +0900

    簡単にイベントを展開できるようにした

--- app/Livewire/Event.php
+++ app/Livewire/Event.php
@@ -2,10 +2,19 @@
 
 namespace App\Livewire;
 
+use App\Models\Event as ModelsEvent;
 use Livewire\Component;
 
 class Event extends Component
 {
+    public $events = [];
+
+    public function mount()
+    {
+        $this->events = ModelsEvent::orderby('start_time', 'asc')->get();
+    }
+
+
     public function render()
     {
         return view('livewire.event');
--- resources/views/livewire/event.blade.php
+++ resources/views/livewire/event.blade.php
@@ -1,3 +1,25 @@
 <div>
-    {{-- The whole world belongs to you. --}}
+    <h2>イベント一覧</h2>
+
+    <table border="1" style="width: 100%; border-collapse: collapse;">
+        <thead>
+            <tr>
+                <th>タイトル</th>
+                <th>開始時間</th>
+                <th>終了時間</th>
+                <th>リマインダー</th>
+                <th>詳細</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($events as $event)
+            <tr></tr>
+                <td>{{ $event->title }}</td>
+                <td>{{ $event->start_time }}</td>
+                <td>{{ $event->end_time }}</td>
+                <td>{{ $event->reservation_time }}</td>
+                <td>{{ $event->detail }}</td>
+            @endforeach($events as $event)
+        </tbody>
+    </table>
 </div>
