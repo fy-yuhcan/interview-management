ad7a78c396d911645f7b0f21ee47667485eea459
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2025-02-23 00:00:39 +0900

    イベント詳細画面

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\SocialiteController;
+use App\Livewire\Event;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,3 +11,4 @@ Route::get('/', function () {
 Route::view('/login', 'auth.login')->name('login');
 Route::get('/redirect/{provider}', [SocialiteController::class, 'redirect'])->name('redirect');
 Route::get('/login/{provider}/callback', [SocialiteController::class, 'callback'])->name('callback');
+Route::get('/events/{event}', [Event::class, 'show'])->name('events.show');
