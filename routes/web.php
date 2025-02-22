fe26aec74bdeb4c4f169c890c91481cc8825dbf5
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2025-02-22 23:53:28 +0900

    ユーザー設定画面

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,6 @@ Route::view('/login', 'auth.login')->name('login');
 Route::get('/redirect/{provider}', [SocialiteController::class, 'redirect'])->name('redirect');
 Route::get('/login/{provider}/callback', [SocialiteController::class, 'callback'])->name('callback');
 Route::get('/events/{event_id}', EventDetail::class)->name('events.show');
+Route::get('/user/settings', function () {
+    return view('user.settings');
+})->middleware('auth')->name('user.settings');
