03c319cb1b3b5004e640e6a3bf4a19edf8d784e3
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2025-02-25 19:27:39 +0900

    openaiのサービスクラスからgoogleカレンダーのcreateEventを呼んでイベントidを取得できるようにした

--- app/Services/EventCreateService.php
+++ app/Services/EventCreateService.php
@@ -3,22 +3,15 @@
 namespace App\Services;
 
 use App\Models\Event;
-use Illuminate\Support\Facades\Auth;
+
 
 class EventCreateService
 {
-    public $googleCalendarService;
-    public function __construct(GoogleCalendarService $googleCalendarService)
+    public function createEvent($formattedResponse, $userId,$createGoogleCalendarEvent)
     {
-        $this->googleCalendarService = $googleCalendarService;
-    }
-
-    public function createEvent($formattedResponse, $userId)
-    {
-        if (empty($formattedResponse['calendar_id'])) {
-            //ここでcalendar_idを設定する
-            $createdEvent = $this->googleCalendarService->createEvent($formattedResponse);
-            $formattedResponse['calendar_id'] = $createdEvent->getId();
+        //ここでgoogleカレンダーのid
+        if (!isset($formattedResponse['calendar_id'])) {
+            $formattedResponse['calendar_id'] = $createGoogleCalendarEvent->getId();
         }
 
         return Event::firstOrCreate(
