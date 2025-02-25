aad29ad8dd080cf5fd5921500a108b25e794281d
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2025-02-25 19:01:42 +0900

    inputから作成したときでもイベントがgoogleカレンダーに作成されるようにする

--- app/Services/EventCreateService.php
+++ app/Services/EventCreateService.php
@@ -3,25 +3,17 @@
 namespace App\Services;
 
 use App\Models\Event;
+use Illuminate\Support\Facades\Auth;
 
 class EventCreateService
 {
     public function createEvent($formattedResponse, $userId)
     {
+        $user = Auth::user();
         if (empty($formattedResponse['calendar_id'])) {
-            return Event::create([
-                'user_id' => $userId,
-                'priority_id' => $formattedResponse['priority_id'] ?? null,
-                'calendar_id' => $formattedResponse['calendar_id'] ?? null,
-                'title' => $formattedResponse['title'] ?? null,
-                'start_time' => $formattedResponse['start_time'] ?? null,
-                'end_time' => $formattedResponse['end_time'] ?? null,
-                'reservation_time' => $formattedResponse['reservation_time'] ?? null,
-                'status' => $formattedResponse['status'] ?? null,
-                'url' => $formattedResponse['url'] ?? null,
-                'detail' => $formattedResponse['detail'] ?? null,
-            ]);
-        } else {
+            //ここでcalendar_idを設定する
+            }
+            
             return Event::firstOrCreate(
                 [
                     'user_id' => $userId,
@@ -41,6 +33,5 @@ class EventCreateService
                     'detail' => $formattedResponse['detail'] ?? null,
                 ]
             );
-        }
     }
 }
