2333865ff0505bd5881009e9c224146c85d358fe
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2025-02-25 19:05:02 +0900

    呼び出してidを作成できるように修正

--- app/Services/EventCreateService.php
+++ app/Services/EventCreateService.php
@@ -7,31 +7,37 @@ use Illuminate\Support\Facades\Auth;
 
 class EventCreateService
 {
+    public $googleCalendarService;
+    public function __construct(GoogleCalendarService $googleCalendarService)
+    {
+        $this->googleCalendarService = $googleCalendarService;
+    }
+
     public function createEvent($formattedResponse, $userId)
     {
-        $user = Auth::user();
         if (empty($formattedResponse['calendar_id'])) {
             //ここでcalendar_idを設定する
-            }
-            
-            return Event::firstOrCreate(
-                [
-                    'user_id' => $userId,
-                    'calendar_id' => $formattedResponse['calendar_id']
-                ],
-                [
-                    'user_id' => $userId,
-                    //TODO:優先度はあとで実装
-                    'priority_id' => $formattedResponse['priority_id'] ?? null,
-                    'calendar_id' => $formattedResponse['calendar_id'] ?? null,
-                    'title' => $formattedResponse['title'] ?? null,
-                    'start_time' => $formattedResponse['start_time'] ?? null,
-                    'end_time' => $formattedResponse['end_time'] ?? null,
-                    'reservation_time' => $formattedResponse['reservation_time'] ?? null,
-                    'status' => $formattedResponse['status'] ?? null,
-                    'url' => $formattedResponse['url'] ?? null,
-                    'detail' => $formattedResponse['detail'] ?? null,
-                ]
-            );
+            $createdEvent = $this->googleCalendarService->createEvent($formattedResponse);
+            $formattedResponse['calendar_id'] = $createdEvent->getId();
+        }
+
+        return Event::firstOrCreate(
+            [
+                'user_id' => $userId,
+                'calendar_id' => $formattedResponse['calendar_id']
+            ],
+            [
+                'user_id' => $userId,
+                //TODO:優先度はあとで実装
+                'priority_id' => $formattedResponse['priority_id'] ?? null,
+                'title' => $formattedResponse['title'] ?? null,
+                'start_time' => $formattedResponse['start_time'] ?? null,
+                'end_time' => $formattedResponse['end_time'] ?? null,
+                'reservation_time' => $formattedResponse['reservation_time'] ?? null,
+                'status' => $formattedResponse['status'] ?? null,
+                'url' => $formattedResponse['url'] ?? null,
+                'detail' => $formattedResponse['detail'] ?? null,
+            ]
+        );
     }
 }
