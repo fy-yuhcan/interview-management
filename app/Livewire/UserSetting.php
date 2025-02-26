8ab4604e614159cdf0031987e6b1031dfd79243b
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2025-02-26 18:18:29 +0900

    更新の関数を追加

--- app/Livewire/UserSetting.php
+++ app/Livewire/UserSetting.php
@@ -2,10 +2,34 @@
 
 namespace App\Livewire;
 
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class UserSetting extends Component
 {
+    public $email;
+
+    public function mount()
+    {
+        $this->email = Auth::user()->email;
+    }
+
+
+    /**
+     * メールアドレスを更新する
+     */
+    public function updateEmail()
+    {
+        /**
+         * @var \App\Models\User $user
+         */
+        $user = Auth::user();
+
+        $user->update([
+            'email' => $this->email,
+        ]);
+    }
+
     public function render()
     {
         return view('livewire.user-setting');
