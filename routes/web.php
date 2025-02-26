f0065e0106265346293833fc0811de3acd9fc2c4
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2025-02-26 18:26:15 +0900

    ルーティングの修正

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,4 @@ Route::view('/login', 'auth.login')->name('login');
 Route::get('/redirect/{provider}', [SocialiteController::class, 'redirect'])->name('redirect');
 Route::get('/login/{provider}/callback', [SocialiteController::class, 'callback'])->name('callback');
 Route::get('/events/{event_id}', EventDetail::class)->name('events.show');
-Route::get('/user/settings', function () {
-    return view('user.settings');
-})->middleware('auth')->name('user.settings');
 Route::get('/user/setting', UserSetting::class)->name('user.setting');
