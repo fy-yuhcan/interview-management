49fd5762113ba0127ffa2f5b7edd1ce78159c761
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2025-03-18 04:15:36 +0900

    composer.lock

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -5,5 +5,6 @@ echo "Running BeforeInstall hook..."
 sudo systemctl stop nginx
 sudo systemctl stop php-fpm
 sudo rm -rf /var/www/html/interview-management/docker-compose.yml
+sudo rm -rf /var/www/html/interview-management/composer.lock
 
 set -e
