1e8dc2c87fd6e2334cd620ba690debbf29ab71f2
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2025-03-18 11:26:15 +0900

    artisan 削除

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -6,5 +6,6 @@ sudo systemctl stop nginx
 sudo systemctl stop php-fpm
 sudo rm -rf /var/www/html/interview-management/docker-compose.yml
 sudo rm -rf /var/www/html/interview-management/composer.lock
+sudo rm -rf /var/www/html/interview-management/artisan
 
 set -e
