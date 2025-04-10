448fa402d16e4085ad0caf3816ff42fd28f856d6
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2025-04-10 13:59:39 +0900

    afterinstallでパラメータストアを参照するように修正

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -4,16 +4,29 @@
 echo "Running AfterInstall hook..."
 
 #環境変数ファイルの準備
-ENV_PROD="/home/ec2-user/env.production"
+# ENV_PROD="/home/ec2-user/env.production"
+
+DB_HOST=$(aws ssm get-parameter --name "/interview-management/DB_HOST" --query "Parameter.Value" --output text)
+DB_USER=$(aws ssm get-parameter --name "/interview-management/DB_USER_NAME" --query "Parameter.Value" --output text)
+DB_PASS=$(aws ssm get-parameter --name "/interview-management/DB_PASS" --query "Parameter.Value" --output text)
+
 ENV="/var/www/html/interview-management/.env"
 
-if [ -f "$ENV_PROD" ]; then
-  cp "$ENV_PROD" "$ENV"
-  echo ".env.production copied successfully."
-else
-  echo "ERROR: .env.production does not exist!" >&2
-  exit 1
-fi
+# if [ -f "$ENV_PROD" ]; then
+#   cp "$ENV_PROD" "$ENV"
+#   echo ".env.production copied successfully."
+# else
+#   echo "ERROR: .env.production does not exist!" >&2
+#   exit 1
+# fi
+
+echo "updateing .env file..."
+
+cat <<EOF > ${ENV}
+DB_HOST=${DB_HOST}
+DB_USER=${DB_USER}
+DB_PASS=${DB_PASS}
+EOF
 
 #Composer依存関係のインストール
 cd /var/www/html/interview-management
